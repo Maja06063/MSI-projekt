5a9bd91e6750a4ce61ea97bf5aaefbb72f499c45
Maja06063 <259707@student.pwr.edu.pl>
2023-04-01 19:10:29 +0200

    Początki implementacji datainitu

--- Program/data_initializer.py
+++ Program/data_initializer.py
@@ -1,2 +1,25 @@
+from sklearn import datasets
+
 class DataInitializer():
-    pass
+#metody, które mają na celu przygotować dane do eksperymentów
+    
+    
+    data_x= [] #dwuwymiarowa tablica cech
+    data_y = [] #jednowymiarowa tablica poprawnych odpowiedzi
+
+    def prepare_artificial_data(self):
+
+        self.data_x, self.data_y = datasets.make_classification(
+            n_samples= 500
+        )
+    #input_file - plik z ktorego bedziemy brac realne
+    def prepare_real_data(self, input_file: str):
+        pass 
+    
+    def get_x_data(self):
+        return self.data_x.copy()
+    
+    def get_y_data(self):
+        return self.data_y.copy()
+
+    
--- Program/experiments.py
+++ Program/experiments.py
@@ -2,6 +2,14 @@ from bagging  import Bagging
 from boosting import Boosting
 from logistic_regression import LogisticRegression
 from random_stumps import RandomStumps
+from data_initializer import DataInitializer
 
 class Experiments():
-    pass
+
+    data_init = DataInitializer ()
+    def __init__(self):
+        pass
+
+    #metoda przeprowadzająca eksperymenty
+    def run(self):
+        pass
