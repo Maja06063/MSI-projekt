ae634a4d3760031df63f7307c024c4d6b0b81d91
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-02 19:02:42 +0200

    Update random_stumps.py

--- Program/random_stumps.py
+++ Program/random_stumps.py
@@ -1,22 +1,40 @@
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
 import numpy as np
+from collections import Counter
+from decision_stump import DecisionStump
 
 class RandomStumps (BaseEstimator, ClassifierMixin):
-    
-    def __init__(self):
-        #Dla Oli: Uzupełnij metodę
-        pass
-
-    def fit(self,x_train,y_train):
-        #Dla Oli: Uzupełnij metodę
-        print("Tu bedzie fit")
+    def __init__(self, n_trees, max_depth=1, min_samples_split=2, n_feature=None):
+        self.n_trees = n_trees
+        self.max_depth=max_depth
+        self.min_samples_split=min_samples_split
+        self.n_features=n_feature
+        self.trees = []
+        
+    def fit(self, X, y):
+        self.trees = []
+        for _ in range(self.n_trees):
+            tree = DecisionStump(max_depth=self.max_depth,
+                            min_samples_split=self.min_samples_split,
+                            n_features=self.n_features)
+            X_sample, y_sample = self._bootstrap_samples(X, y)
+            tree.fit(X_sample, y_sample)
+            self.trees.append(tree)
         return self
 
-    def predict(self,x_test):
-        #Dla Oli: Uzupełnij metodę
-        print("Tu bedzie predict")
-        #place_holder tymczasowo zwraca same 0, aby program nie wywalał się, bo jeszcze nie ma 
-        #napisanego predicta
-        place_holder = [0] * len(x_test)
-        return place_holder
+    def _bootstrap_samples(self, X, y):
+        n_samples = X.shape[0]
+        idxs = np.random.choice(n_samples, n_samples, replace=True)
+        return X[idxs], y[idxs]
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
+    def predict(self, X):
+        predictions = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(predictions, 0, 1)
+        predictions = np.array([self._most_common_label(pred) for pred in tree_preds])
+        return predictions
