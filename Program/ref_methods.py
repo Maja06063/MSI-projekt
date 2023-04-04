57ca970238a425e4e958015511e5b70899e988dc
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 23:49:28 +0200

    Update ref_methods.py

--- Program/ref_methods.py
+++ Program/ref_methods.py
@@ -2,6 +2,10 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.linear_model import LogisticRegression
 
+"""
+Klasa RefMethods służy do przechowywania metod referencyjnych.
+"""
+
 class RefMethods():
     bagging = BaggingClassifier ()
     boosting = GradientBoostingClassifier ()
