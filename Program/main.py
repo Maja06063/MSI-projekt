1250e11273010b2cff9221d8446d5ae652ebc54d
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 23:48:28 +0200

    Update main.py

--- Program/main.py
+++ Program/main.py
@@ -1,8 +1,21 @@
 from experiments import Experiments
 
+"""
+Program do przeprowadzania eksperymentów na różnych algorytmach sztucznej inteligencji.
+Dostępne algorytmy:
+- RandomStumps,
+- Boosting,
+- Bagging,
+- Regresja logistyczna.
+
+Autorki:
+- Aleksandra Gadzińska,
+- Maja Skibińska.
+"""
+
 # Aby nie można było importować maina w innych plikach
 if __name__ == "__main__":
     experiments = Experiments()
-    experiments.run()
+    experiments.run()\
 
     
