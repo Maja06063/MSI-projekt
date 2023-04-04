3467e293ba6cc0a4cd11df344badcf80e9c2642f
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 17:04:26 +0200

    Update decision_sutmp with comments

--- Program/decision_stump.py
+++ Program/decision_stump.py
@@ -3,15 +3,16 @@ from sklearn.base import ClassifierMixin
 import numpy as np
 from collections import Counter
 
+# node'y (pola decyzji - takie if'y gdzie nastepuje podzial zbioru- w pniu tylko jeden taki Node)
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
+    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None): #* powoduje ze trzeba zrobic Node.value
         self.feature = feature
-        self.threshold = threshold
+        self.threshold = threshold  #prog decyzyjny przy ktorym koniec decyzji o podziale
         self.left = left
         self.right = right
-        self.value = value
+        self.value = value 
         
-    def is_leaf_node(self):
+    def is_leaf_node(self): #sprawdzanie czy ten node jest lisciem
         return self.value is not None
 
 class DecisionStump (BaseEstimator, ClassifierMixin):
@@ -19,7 +20,7 @@ class DecisionStump (BaseEstimator, ClassifierMixin):
     def __init__(self, min_samples_split=2, max_depth=1, n_features=None):
         self.min_samples_split=min_samples_split
         self.max_depth=max_depth
-        self.n_features=n_features
+        self.n_features=n_features #randomowosc przy podzbiorach
         self.root=None
         
     def fit(self, X, y):
