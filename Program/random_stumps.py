5b3be8c51946ca1edd6f767f235e90cdc66a9ab5
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 23:46:02 +0200

    Update random_stumps.py

--- Program/random_stumps.py
+++ Program/random_stumps.py
@@ -4,14 +4,32 @@ import numpy as np
 from collections import Counter
 from decision_stump import DecisionStump
 
+"""
+Klasa RandomStumps jest klasyfikatorem implementowanym w zadaniu.
+Posiada konstruktor oraz metodę fit i predict zgodne z innymi klasyfikatorami biblioteki sklearn.
+"""
+
 class RandomStumps (BaseEstimator, ClassifierMixin):
-    def __init__(self, n_stumps, max_depth=1, min_samples_split=2, n_feature=None):
+
+    """
+    Konstruktor budujący obiekt klasyfikatora.
+    Możliwe parametry:
+    TODO!!!
+    """
+    def __init__(self, n_stumps=2, max_depth=1, min_samples_split=2, n_feature=None):
         self.n_stumps = n_stumps
         self.max_depth=max_depth
         self.min_samples_split=min_samples_split
         self.n_features=n_feature
         self.stumps = []
-        
+
+    """
+    Metoda fit, za pomocą której odbywa się całe uczenie algorytmu - budowanie lasu stumpów.
+    Argumenty:
+    1. x - dwuwymiarowa tablica. Pierwszy wymiar mówi o numerze próbki, drugi o numerze cechy,
+    2. y - tablica zawierająca poparawne wyniki klasyfikacji cech dla danej próbki,
+        na jej podstawie algorytm uczy się.
+    """
     def fit(self, X, y):
         self.stumps = []
         for _ in range(self.n_stumps):
@@ -23,16 +41,31 @@ class RandomStumps (BaseEstimator, ClassifierMixin):
             self.stumps.append(stump)
         return self
 
+    """
+    Metoda _bootstrap_samples jest metodą pomocniczą.
+    """
     def _bootstrap_samples(self, X, y):
         n_samples = X.shape[0]
         idxs = np.random.choice(n_samples, n_samples, replace=True)
         return X[idxs], y[idxs]
 
+    """
+    Metoda _most_common_label jest metodą pomocniczą.
+    Zwraca najbardziej popularną klasę.
+    """
     def _most_common_label(self, y):
         counter = Counter(y)
         most_common = counter.most_common(1)[0][0]
         return most_common
 
+    """
+    Metoda predict służąca do przeprowadzenia klasyfikacji - gdy algorytm jest już nauczony metodą fit,
+    używamy predict aby podejmował decyzję.
+    Argumenty:
+    1. x - dwuwymiarowa tablica. Pierwszy wymiar mówi o numerze próbki, drugi o numerze cechy,
+        algorytm ma za zadanie dla każdego obiektu z tablicy przydzielić do jakiej klasy należy.
+    Metoda zwraca tablicę zawierającą klasy obiektów ("wymyślone" przez algorytm).
+    """
     def predict(self, X):
         predictions = np.array([stump.predict(X) for stump in self.stumps])
         stump_preds = np.swapaxes(predictions, 0, 1)
