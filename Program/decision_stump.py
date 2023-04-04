3a7911a3886a7e41401fc62acbc542ab4c779963
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 23:21:47 +0200

    Update decision_stump.py

--- Program/decision_stump.py
+++ Program/decision_stump.py
@@ -34,59 +34,59 @@ class DecisionStump (BaseEstimator, ClassifierMixin):
         self.root=None
 
     def fit(self, X, y):
-        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features)
-        self.root = self._grow_stump(X, y)
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features) #sprawdzanie czy nie przekrocza tych cech co mamy
+        self.root = self.grow_stump(X, y)
         return self
 
     """
     Important!
     Tutaj jest właśnie problem, czy chcemy mieć depth=0, czy depth=1???
     """
-    def _grow_stump(self, X, y, depth=0):    #tworzenie drzewa, u nas drzewo jest z jedna decyzja, jeden poziom glebokosci, dlatego tez depth = 1 i tylko raz zbuduje rozgalezienia (dwa liscie)
-        n_samples, n_feats = X.shape
+    def grow_stump(self, X, y, depth=0):    #tworzenie drzewa, u nas drzewo jest z jedna decyzja, jeden poziom glebokosci, dlatego tez depth = 1 i tylko raz zbuduje rozgalezienia (dwa liscie)
+        n_samples, n_feature = X.shape
         n_labels = len(np.unique(y))
 
         # sprawdzanie kryterium budowania drzewa (drzewo z jedna decyzja bedzie)
         if (depth>=self.max_depth or n_labels==1 or n_samples<self.min_samples_split):
-            leaf_value = self._most_common_label(y)
+            leaf_value = self.most_common_label(y)
             return Node(value=leaf_value)
 
-        feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
+        feature_idxs = np.random.choice(n_feature, self.n_features, replace=False)
 
-        # znalezienie najlepszy podzial na *liscie*
-        best_feature, best_thresh = self._best_split(X, y, feat_idxs)
+        # znalezienie najlepszy podzial na te *liscie*
+        best_feature, best_thresh = self.best_split(X, y, feature_idxs)
 
-        # stworzy sie jedno rozgalezienie (ze ten decyzyjny node i jedno na lewo i na prawo decyzja)
-        left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
-        left = self._grow_stump(X[left_idxs, :], y[left_idxs], depth+1)
-        right = self._grow_stump(X[right_idxs, :], y[right_idxs], depth+1)
+        # create child nodes - stworzy sie tylko jakby jedno rozgalezienie (ze ten decyzyjny node i jedno na lewo i na prawo decyzja)
+        left_idxs, right_idxs = self.split(X[:, best_feature], best_thresh)
+        left = self.grow_stump(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self.grow_stump(X[right_idxs, :], y[right_idxs], depth+1)
         return Node(best_feature, best_thresh, left, right)
 
-    def _best_split(self, X, y, feat_idxs):
+    def best_split(self, X, y, feature_idxs):
         best_gain = -1
         split_idx, split_threshold = None, None
 
-        for feat_idx in feat_idxs:
-            X_column = X[:, feat_idx]
+        for feature_idx in feature_idxs:
+            X_column = X[:, feature_idx]
             thresholds = np.unique(X_column)
 
             for thr in thresholds:
                 # obliczanie zysku informacji (im wiekszy tym lepszy jest ten klasyfikator - drzewo)
-                gain = self._information_gain(y, X_column, thr)
+                gain = self.information_gain(y, X_column, thr)
 
                 if gain > best_gain:
                     best_gain = gain
-                    split_idx = feat_idx
+                    split_idx = feature_idx
                     split_threshold = thr
 
         return split_idx, split_threshold
 
-    def _information_gain(self, y, X_column, threshold):
+    def information_gain(self, y, X_column, threshold):
         # entropia rodzica
-        parent_entropy = self._entropy(y)
+        parent_entropy = self.entropy(y)
 
         # tworzenie dzieci do entropii
-        left_idxs, right_idxs = self._split(X_column, threshold)
+        left_idxs, right_idxs = self.split(X_column, threshold)
 
         if len(left_idxs) == 0 or len(right_idxs) == 0:
             return 0
@@ -94,36 +94,36 @@ class DecisionStump (BaseEstimator, ClassifierMixin):
         # obliczanie wag wartosci entropii dzieci
         n = len(y)
         n_l, n_r = len(left_idxs), len(right_idxs)
-        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        e_l, e_r = self.entropy(y[left_idxs]), self.entropy(y[right_idxs])
         child_entropy = (n_l/n) * e_l + (n_r/n) * e_r
 
         # obliczanie zysku informacyjnego
         information_gain = parent_entropy - child_entropy
         return information_gain
 
-    def _split(self, X_column, split_thresh):
+    def split(self, X_column, split_thresh):
         left_idxs = np.argwhere(X_column <= split_thresh).flatten()
         right_idxs = np.argwhere(X_column > split_thresh).flatten()
         return left_idxs, right_idxs
 
-    def _entropy(self, y):
+    def entropy(self, y):
         hist = np.bincount(y)
         ps = hist / len(y)
         return -np.sum([p * np.log(p) for p in ps if p>0])
 
 
-    def _most_common_label(self, y):
+    def most_common_label(self, y):
         counter = Counter(y)
         value = counter.most_common(1)[0][0]
         return value
 
     def predict(self, X):
-        return np.array([self._traverse_stump(x, self.root) for x in X])
+        return np.array([self.traverse_stump(x, self.root) for x in X])
 
-    def _traverse_stump(self, x, node):
+    def traverse_stump(self, x, node):
         if node.is_leaf_node():
             return node.value
 
         if x[node.feature] <= node.threshold:
-            return self._traverse_stump(x, node.left)
-        return self._traverse_stump(x, node.right)
+            return self.traverse_stump(x, node.left)
+        return self.traverse_stump(x, node.right)
