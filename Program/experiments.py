a17613fc3596d027f22de5557273cb25183af70f
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-04 23:48:01 +0200

    Update experiments.py

--- Program/experiments.py
+++ Program/experiments.py
@@ -4,6 +4,10 @@ from data_initializer import DataInitializer
 from sklearn.metrics import accuracy_score
 from ref_methods import RefMethods
 
+"""
+Klasa Experiments służy do przeprowadzenia ekperymentów. Składa się przede wszystkim z metody run.
+"""
+
 class Experiments():
 
     data_init = DataInitializer ()
@@ -11,8 +15,12 @@ class Experiments():
     def __init__(self):
         pass
 
-    #metoda przeprowadzająca eksperymenty
+    """
+    Metoda run przeprowadza eksperymenty.
+    """
     def run(self):
+
+        # Wybór danych wejściowych do algorytmu:
         while True:
             print("1.Symetryczne\n2.Realne\n3.Oba\n4.Wyjscie\n")
             data_input = input("Które dane neleży użyć? Wciśnij 1, 2 lub 3\n")
@@ -31,58 +39,72 @@ class Experiments():
             else:
                 print("Niepoprawny wybor!Sprobuj jeszcze raz\n")
 
-
+        # Pobranie tych danych do metody run:
         data_x = self.data_init.get_x_data_copy()
         data_y = self.data_init.get_y_data_copy()
 
+        # Podział na zbiór uczący i testowy. Docelowo będzie zastąpiony walidacją krzyżową:
         x_train, x_test, y_train, y_test = train_test_split(
-        data_x,data_y,
-        test_size = 0.2,
-        random_state = 66
+            data_x,data_y,
+            test_size = 0.2,
+            random_state = 66
         )
 
-
+        # Wybór algorytmu do uruchomienia:
         algorithms_loop = True
         while algorithms_loop:
             print("1.Random Stumps\n2.boosting\n3.bagging\n4.Regresja Logistyczna\n5.Wyjscie z programu\n")
             data_input = input("Ktory algorytm uruchomic?Podaj numery algorytmow, bez spacji!\nPrzykład:13\n")
 
+            # Wyjście z programu:
             if "5" in data_input:
                 exit(0)
 
+            # Random Stumps:
             if "1" in data_input:
                 algorithms_loop = False
                 print("Uruchomiono algorytm Random Stumps")
-                random_stumps_algorithm = RandomStumps(len(x_train[0]))
+
+                n_classes = max(y_train) + 1
+                random_stumps_algorithm = RandomStumps(n_classes)
                 random_stumps_algorithm.fit(x_train,y_train)
                 y_predict = random_stumps_algorithm.predict(x_test)
+
                 score = accuracy_score(y_test,y_predict)
                 print("Metryka klasyfikacji Random Stumps wynosi:"+str(score))
 
+            # Boosting
             if "2" in data_input:
                 algorithms_loop = False
                 print("Uruchomiono algorytm boosting")
+
                 boosting_algorithm = self.ref_methods.boosting
                 boosting_algorithm.fit(x_train,y_train)
                 y_predict = boosting_algorithm.predict(x_test)
+
                 score = accuracy_score(y_test,y_predict)
                 print("Metryka klasyfikacji Boosting wynosi:"+str(score))
-               
 
+            # Bagging:
             if "3" in data_input:
                 algorithms_loop = False
                 print("Uruchomiono algorytm bagging")
+
                 bagging_algorithm = self.ref_methods.bagging
                 bagging_algorithm.fit(x_train,y_train)
                 y_predict = bagging_algorithm.predict(x_test)
+
                 score = accuracy_score(y_test,y_predict)
                 print("Metryka klasyfikacji Bagging wynosi:"+str(score))
 
+            # Regresja logistyczna:
             if "4" in data_input:
                 algorithms_loop = False
                 print("Uruchomiono algorytm regresji logistycznej")
+
                 log_reg_algorithm = self.ref_methods.logistic_regression
                 log_reg_algorithm.fit(x_train,y_train)
                 y_predict = log_reg_algorithm.predict(x_test)
+
                 score = accuracy_score(y_test,y_predict)
                 print("Metryka klasyfikacji regresji logistycznej wynosi:"+str(score))
