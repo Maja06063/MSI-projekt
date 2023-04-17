33dac030ce04c049f6deb329d300b4edf576ebb9
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-04-17 15:59:42 +0200

    Experiments z walidacja i wykresami

--- Program/experiments.py
+++ Program/experiments.py
@@ -7,6 +7,7 @@ from show_results import ShowResults
 from random_state import RANDOM_STATE
 from sklearn.model_selection import RepeatedStratifiedKFold
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Klasa Experiments służy do przeprowadzenia ekperymentów. Składa się przede wszystkim z metody run.
@@ -29,6 +30,7 @@ class Experiments():
         while True:
             print("1.Syntetyczne\n2.Realne\n3.Oba\n4.Wyjscie\n")
             data_input = input("Które dane neleży użyć? Wciśnij 1, 2 lub 3\n")
+            data_for_plots = data_input # zmienna do warunku zeby wykresy tylko do syntetycznych robic
             if data_input == "1":
                 self.data_init.prepare_artificial_data()
                 break
@@ -51,6 +53,7 @@ class Experiments():
         # Implementacja funckji do podziału za pomocą startyfikowanej wielokttonej walidacji krzyżowej 5:2
         splits = 5
         repeats = 2
+        iter = range(1,11)
         rskf = RepeatedStratifiedKFold(n_splits= splits, n_repeats= repeats, random_state= RANDOM_STATE)
         
 
@@ -78,10 +81,26 @@ class Experiments():
                     random_stumps_algorithm.fit(x_train,y_train)
                     y_predict = random_stumps_algorithm.predict(x_test)
                     scores.append(accuracy_score(y_test, y_predict))
-
+                
+                # Wyswietlanie prcesu nauki dla kazdego folda
+                fig0 = plt.figure('Figure 0')
+                plt.plot(iter, scores)
+                plt.title('Random Stumps metryka a foldy')
+                plt.savefig('../RS_foldy')
+                
                 mean_score = np.mean(scores)
                 std_score = np.std(scores)
                 print("Metryka dokladnosci klasyfikacji Random Stumps wynosi: %.3f (%.3f)" % (mean_score, std_score))
+                
+                # Wyswietlanie subplotow dla zbiory syntetycznego dla prawdziwej etykiety (po lewej) i predykcji (po prawej)
+                if data_for_plots == "1":
+                    fig1 = plt.figure('Figure 1')
+                    fig, ax = plt.subplots(1,2)
+                    fig.suptitle("Random Stumps")
+                    ax[0].scatter(x_test[:, 0], x_test[:, 1], c= y_test, cmap= 'bwr')
+                    ax[1].scatter(x_test[:, 0], x_test[:, 1], c= y_predict, cmap= 'bwr')
+                    plt.savefig('../Random_Stumps.png') 
+
                 self.show_results.random_stumps_score = mean_score
 
             # Boosting
@@ -97,9 +116,26 @@ class Experiments():
                     y_predict = boosting_algorithm.predict(x_test)
                     scores.append(accuracy_score(y_test, y_predict))
 
+                # Wyswietlanie prcesu nauki dlakazdego folda
+                fig2 = plt.figure('Figure 2')
+                plt.plot(iter, scores)
+                plt.title('Boosting metryka a foldy')
+                plt.savefig('../Boost_foldy')
+                #plt.show()
+                
                 mean_score = np.mean(scores)
                 std_score = np.std(scores)
                 print("Metryka klasyfikacji Boosting wynosi: %.3f (%.3f)" % (mean_score, std_score))
+
+                # Wyswietlanie subplotow dla zbiory syntetycznego dla prawdziwej etykiety (po lewej) i predykcji (po prawej)
+                if data_for_plots == "1":
+                    fig3 = plt.figure('Figure 3')
+                    fig, ax = plt.subplots(1,2)
+                    fig.suptitle("Boosting")
+                    ax[0].scatter(x_test[:, 0], x_test[:, 1], c= y_test, cmap= 'bwr')
+                    ax[1].scatter(x_test[:, 0], x_test[:, 1], c= y_predict, cmap= 'bwr')
+                    plt.savefig('../Boosting.png')
+
                 self.show_results.boosting_score = mean_score
 
             # Bagging:
@@ -115,9 +151,25 @@ class Experiments():
                     y_predict = bagging_algorithm.predict(x_test)
                     scores.append(accuracy_score(y_test, y_predict))
 
+                # Wyswietlanie prcesu nauki dla kazdego folda
+                fig4 = plt.figure('Figure 4')
+                plt.plot(iter, scores)
+                plt.title('Bagging metryka a foldy')
+                plt.savefig('../Bagg_foldy')
+
                 mean_score = np.mean(scores)
                 std_score = np.std(scores)
                 print("Metryka klasyfikacji Bagging wynosi: %.3f (%.3f)" % (mean_score, std_score))
+                
+                # Wyswietlanie subplotow dla zbiory syntetycznego dla prawdziwej etykiety (po lewej) i predykcji (po prawej)
+                if data_for_plots == "1":
+                    fig5 = plt.figure('Figure 5')
+                    fig, ax = plt.subplots(1,2)
+                    fig.suptitle("Bagging")
+                    ax[0].scatter(x_test[:, 0], x_test[:, 1], c= y_test, cmap= 'bwr')
+                    ax[1].scatter(x_test[:, 0], x_test[:, 1], c= y_predict, cmap= 'bwr')
+                    plt.savefig('../Bagging.png')
+                
                 self.show_results.bagging_score = mean_score
 
             # Regresja logistyczna:
@@ -133,9 +185,25 @@ class Experiments():
                     y_predict = log_reg_algorithm.predict(x_test)
                     scores.append(accuracy_score(y_test, y_predict))
 
+                # Wyswietlanie prcesu nauki dlakazdego folda
+                fig6 = plt.figure('Figure 6')
+                plt.plot(iter, scores)
+                plt.title('Regresja logistyczna metryka a foldy')
+                plt.savefig('../RL_foldy')
+
                 mean_score = np.mean(scores)
                 std_score = np.std(scores)
                 print("Metryka klasyfikacji regresji logistycznej wynosi: %.3f (%.3f)" % (mean_score, std_score))
+                
+                # Wyswietlanie subplotow dla zbiory syntetycznego dla prawdziwej etykiety (po lewej) i predykcji (po prawej)
+                if data_for_plots == "1":
+                    fig7 = plt.figure('Figure 7')
+                    fig, ax = plt.subplots(1,2)
+                    fig.suptitle("Regresja logistyczna")
+                    ax[0].scatter(x_test[:, 0], x_test[:, 1], c= y_test, cmap= 'bwr')
+                    ax[1].scatter(x_test[:, 0], x_test[:, 1], c= y_predict, cmap= 'bwr')
+                    plt.savefig('../Regresja_logistyczna.png')
+                
                 self.show_results.logistic_regression_score = mean_score
 
         self.show_results.write_results_to_file('../output_file')
